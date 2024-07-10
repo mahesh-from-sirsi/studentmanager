70df27d0caa7bf2371c9c2c829660a6ea0da4f20
seventhbuddha <maheshvenkateshshet@Maheshs-MacBook-Air.local>
2024-07-10 21:16:43 +0530

    Student Manager Spring Boot App - First Commit

---
+++ src/test/java/com/mvs/studentmanager/StudentmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvs.studentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
